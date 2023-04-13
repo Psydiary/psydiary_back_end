36bbae727bd06edb7012558e01853d33e55d8115
Bobby Luly <bobbyluly@gmail.com>
2023-04-13 12:11:25 -0600

    Delete address validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,6 @@ describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
-    it { should validate_presence_of :address }
     it { should validate_presence_of :password }
     it { should validate_uniqueness_of :email }
     it { should have_secure_password}
