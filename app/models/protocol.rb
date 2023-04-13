82ac5ffd71a6d876e05c672ae62aa2cde85e9dd2
Bobby Luly <bobbyluly@gmail.com>
2023-04-12 21:15:09 -0600

    Add test for custom validation

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -2,7 +2,8 @@ class Protocol < ApplicationRecord
   validates_presence_of :name,
                         :dosage,
                         :protocol_duration,
-                        :break_duration
+                        :break_duration,
+                        :description
   validates_numericality_of :days_between_dose, allow_nil: true
   validates_numericality_of :dosage
   validate :daysbetween_xor_dosedays
@@ -11,9 +12,9 @@ class Protocol < ApplicationRecord
 
   private
 
-    def daysbetween_xor_dosedays
-      return if days_between_dose.blank? ^ dose_days.blank?
+  def daysbetween_xor_dosedays
+    return if days_between_dose.blank? ^ dose_days.blank?
 
-      errors.add(:base, 'Specify number of days between dose or days of the week you would like to dose, not both')
-    end
+    errors.add(:base, 'Specify number of days between dose or days of the week you would like to dose, not both')
+  end
 end
--- spec/models/protocol_spec.rb
+++ spec/models/protocol_spec.rb
@@ -10,8 +10,25 @@ describe Protocol do
     it { should validate_presence_of :dosage }
     it { should validate_numericality_of :dosage }
     it { should validate_numericality_of :days_between_dose }
-    xit { should validate_presence_of :daysbetween_xor_dosedays }
     it { should validate_presence_of :protocol_duration }
     it { should validate_presence_of :break_duration }
+    it { should validate_presence_of :description }
+
+    context "#daysbetween_xor_dosedays custom validation" do
+      let(:protocol) { Protocol.create(name: "Test Protocol", description: "This is a test protocol", dosage: 1.5, days_between_dose: 2, dose_days: "Monday, Wednesday, Friday", protocol_duration: "1 month", break_duration: "1 week") }
+
+      it "returns an error if both days_between_dose and dose_days are present" do
+        expect(protocol).to be_invalid
+        expect(protocol.errors.full_messages).to eq(["Specify number of days between dose or days of the week you would like to dose, not both"])
+      end
+
+      it "returns an error if neither days_between_dose and dose_days are present" do
+        protocol.days_between_dose = nil
+        protocol.dose_days = nil
+
+        expect(protocol).to be_invalid
+        expect(protocol.errors.full_messages).to eq(["Specify number of days between dose or days of the week you would like to dose, not both"])
+      end
+    end
   end
 end
