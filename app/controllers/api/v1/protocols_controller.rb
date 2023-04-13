4dc03dc008920e03fc69aa1c90e9ee4ee44e1a8c
Bobby Luly <116703107+Bobsters986@users.noreply.github.com>
2023-04-13 17:28:33 -0600

    BE | Create `ProtocolsController#index` feature and tests (#11)
    * Test: create tests for protocols index
    
    * Feat: create route and controller action for a protocols index

--- app/controllers/api/v1/protocols_controller.rb
+++ app/controllers/api/v1/protocols_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::ProtocolsController < ApplicationController
+  def index
+    render json: ProtocolSerializer.new(Protocol.all)
+  end
+
   def show
     render json: ProtocolSerializer.new(Protocol.find(params[:id]))
   end
