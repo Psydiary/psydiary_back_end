b94121fedd3b2c9c5c0e85922598c62ccd6a9cc7
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-04-13 09:43:05 -0600

    Add: data_sharing to users

---
+++ db/migrate/20230413153941_add_data_sharing_to_users.rb
@@ -0,0 +1,5 @@
+class AddDataSharingToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :data_sharing, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_191633) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_153941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,7 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_191633) do
     t.bigint "protocol_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "data_sharing"
     t.index ["protocol_id"], name: "index_users_on_protocol_id"
   end
-
 end
