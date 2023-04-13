a7e985083f898dbc5e24f3261cf2c3ecb2ec980d
Bobby Luly <bobbyluly@gmail.com>
2023-04-13 17:21:35 -0600

    Test: create tests for protocols index

--- spec/requests/api/v1/protocols/protocols_request_spec.rb
+++ spec/requests/api/v1/protocols/protocols_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Protocols API", type: :request do
   context "#show" do
-    let(:protocol) { Protocol.create!(name: "Test Protocol", description: "This is a test protocol", dosage: 1.5, dose_days: "Monday, Wednesday, Friday", protocol_duration: "1 month", break_duration: "1 week") }
+    let(:protocol) { Protocol.create!(name: "Test Protocol", description: "This is a test protocol", dosage: 1.5, dose_days: "Monday, Wednesday, Friday", protocol_duration: 4, break_duration: 1, other_notes: "Taken in the evening") }
 
     context "when successful" do
       it "returns a protocol" do
@@ -15,6 +15,7 @@ RSpec.describe "Protocols API", type: :request do
         expect(protocol_response[:data].keys).to eq([:id, :type, :attributes])
         expect(protocol_response[:data][:id]).to eq(protocol.id.to_s)
         expect(protocol_response[:data][:type]).to eq("protocol")
+        expect(protocol_response[:data][:attributes].size).to eq(8)
         expect(protocol_response[:data][:attributes][:name]).to eq(protocol.name)
         expect(protocol_response[:data][:attributes][:description]).to eq(protocol.description)
         expect(protocol_response[:data][:attributes][:dosage]).to eq(protocol.dosage)
@@ -22,6 +23,7 @@ RSpec.describe "Protocols API", type: :request do
         expect(protocol_response[:data][:attributes][:dose_days]).to eq(protocol.dose_days)
         expect(protocol_response[:data][:attributes][:protocol_duration]).to eq(protocol.protocol_duration)
         expect(protocol_response[:data][:attributes][:break_duration]).to eq(protocol.break_duration)
+        expect(protocol_response[:data][:attributes][:other_notes]).to eq(protocol.other_notes)
       end
     end
 
@@ -33,6 +35,30 @@ RSpec.describe "Protocols API", type: :request do
 
         expect(response).to have_http_status(404)
         expect(parsed[:errors]).to eq("Couldn't find Protocol with 'id'=986986")
+      end
+    end
+  end
+
+  context "#index" do
+    let!(:protocol) { Protocol.create!(name: "Fadiman", description: "This is a test protocol", days_between_dose: 3, dosage: 0.2, protocol_duration: 4, break_duration: 3, other_notes: "Taken in the morning") }
+    let!(:protocol_2) { Protocol.create!(name: "Stamets", description: "This is the other protocol", dose_days:"Thursday, Friday, Saturday, Sunday", dosage: 0.1, protocol_duration: 4, break_duration: 4, other_notes: "Take with 500mg of Lion's Mane extract powder and 100mg of Niacin Vit B3") }
+    let!(:protocol_3) { Protocol.create!(name: "Nightcap", description: "Yet another protocol", days_between_dose: 3, dosage: 0.2, protocol_duration: 4, break_duration: 3, other_notes: "Taken in the evening") }
+
+    context "when successful" do
+      it "returns a list of all protocols" do
+        get "/api/v1/protocols"
+
+        expect(response).to be_successful
+
+        protocol_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(protocol_response[:data]).to be_an(Array)
+        expect(protocol_response[:data].size).to eq(3)
+        expect(protocol_response[:data][0].keys).to eq([:id, :type, :attributes])
+        expect(protocol_response[:data][0][:attributes].size).to eq(8)
+        expect(protocol_response[:data][0][:attributes][:name]).to eq(protocol.name)
+        expect(protocol_response[:data][1][:attributes][:name]).to eq(protocol_2.name)
+        expect(protocol_response[:data][2][:attributes][:name]).to eq(protocol_3.name)
       end
     end
   end
