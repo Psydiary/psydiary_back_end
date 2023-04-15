e7a788ea6176cafc9ca49ef42a1c0bc47db6135b
Bobby Luly <116703107+Bobsters986@users.noreply.github.com>
2023-04-15 12:37:09 -0600

    Bobby db seeds (#21)
    * Add microdose_logs and daily logs for user.id=2
    
    * Add enums for daily_log_entry exercise and sociability

--- app/models/daily_log_entry.rb
+++ app/models/daily_log_entry.rb
@@ -1,3 +1,6 @@
 class DailyLogEntry < ApplicationRecord
   belongs_to :user
+
+  enum exercise: %w(cardio yoga weights cycling climbing running)
+  enum sociability: %w(social anxious withdrawn)
 end
