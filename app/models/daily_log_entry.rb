fb57b7b50d4a57badaceebbdf91458a8169a5d39
Tori Enyart <torienyart@gmail.com>
2023-04-16 16:21:17 -0600

    Write testing for new daily log entry

--- app/models/daily_log_entry.rb
+++ app/models/daily_log_entry.rb
@@ -1,6 +1,7 @@
 class DailyLogEntry < ApplicationRecord
   belongs_to :user
 
+  validates_presence_of :user_id
   enum exercise: %w(aerobic strength cardio yoga sport)
   enum sociability: %w(social anxious withdrawn)
 end
--- spec/requests/api/v1/daily_log_entries/daily_log_entry_request_spec.rb
+++ spec/requests/api/v1/daily_log_entries/daily_log_entry_request_spec.rb
@@ -70,4 +70,49 @@ RSpec.describe "DailyLogEntry API", type: :request do
       end
     end
   end
+
+  describe "#create" do
+    before :each do
+      @p1 = create(:protocol)
+      @u1 = create(:user, protocol_id: @p1.id, ip_address: "73.153.161.252")
+      @u2 = create(:user, protocol_id: @p1.id, ip_address: "73.153.161.252")
+      @dle1 = create(:daily_log_entry, user_id: @u1.id)
+      @dle2 = create(:daily_log_entry, user_id: @u1.id)
+      @dle3 = attributes_for(:daily_log_entry, user_id: @u1.id)
+      @dle4 = attributes_for(:daily_log_entry)
+    end
+
+    context "when successful" do
+      it "creates a new microdose log entry" do
+        post "/api/v1/users/#{@u1.id}/daily_log_entries", params: @dle3
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(MicrodoseLogEntry.count).to eq(3)
+        expect(response).to be_successful
+        expect(response.status).to eq(201)
+
+
+        expect(microdose_json[:data][:type]).to eq("daily_log_entry")
+        expect(microdose_json[:data][:attributes].keys).to eq([:id, :type, :attributes])
+        expect(microdose_json[:data][:attributes][:user_id]).to eq(@u1)
+        expect(microdose_json[:data][:attributes][:journal_entry]).to eq(@dle3[:journal_entry])
+      end
+    end
+
+    context "when unsuccessful" do
+      it "returns a 422 error if the request is missing a required field or invalid" do
+        post "/api/v1/users/#{@u1.id}/daily_log_entries", params: @dle4
+        json = JSON.parse(response.body, symbolize_names: true)
+
+
+        expect(MicrodoseLogEntry.count).to eq(2)
+      
+        expect(response).to_not be_successful
+        expect(response.status).to eq(422)
+
+        expect(microdose_json[:errors]).to eq(["User Id must exist"])
+        expect(microdose_json[:message]).to eq("Microdose log entry was not created. Please enter valid attributes")
+      end
+    end
+  end
 end
